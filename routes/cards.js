76606e91e2fa636086646c62bb76d96061f60fe4
Scott Nelson <Scott.D.Nelson@dartmouth.edu>
2016-03-18 19:23:58 -0700

    start api for getting/creating cards

---
+++ routes/cards.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+
+var monk = require('monk');
+var db = monk('localhost:27017/vidzy');
+
+router.get('/', function(req, res) {
+  var collection = db.get('cards');
+
+  collection.find({}, function(err, cards) {
+    if (err) throw err;
+
+    res.json(cards);
+  });
+});
+
+router.post('/', function(req, res) {
+  var collection = db.get('cards');
+  var body = req.body;
+
+  collection.insert({
+    name: body.name,
+    email: body.email
+  }, function(err, card) {
+    if (err) throw err;
+
+    res.json(cards);
+  });
+});
+
+module.exports = router;
