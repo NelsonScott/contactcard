f07a12a92b78fe0047654759508a576039608c8c
Scott Nelson <Scott.D.Nelson@dartmouth.edu>
2016-03-19 16:35:10 -0700

    Add home ctrl, query for cards in db to display

--- public/javascripts/contactcard.js
+++ public/javascripts/contactcard.js
@@ -3,13 +3,22 @@ var app = angular.module('Contactcard', ['ngResource', 'ngRoute']);
 app.config(['$routeProvider', function($routeProvider) {
   $routeProvider
     .when('/', {
-      templateUrl: 'partials/home.html'
+      templateUrl: 'partials/home.html',
+      controller: 'homeCtrl'
     })
     .when('/new-card', {
       templateUrl: 'partials/new-card.html',
       controller: 'newCardCtrl'
     });
 }]);
+
+app.controller('homeCtrl', ['$scope', '$resource',
+  function($scope, $resource, $location) {
+    var Cards = $resource('api/cards');
+    Cards.query(function(cards) {
+      $scope.cards = cards;
+    });
+  }]);
 
 app.controller('newCardCtrl', ['$scope', '$resource', '$location',
   function($scope, $resource, $location) {
