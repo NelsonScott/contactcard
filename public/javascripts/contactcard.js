1720b27889933882e6d1b84dea0b6b22e99a87a5
Scott Nelson <Scott.D.Nelson@dartmouth.edu>
2016-03-18 19:23:00 -0700

    use home partial for landing page

--- public/javascripts/contactcard.js
+++ public/javascripts/contactcard.js
@@ -1,1 +1,8 @@
 var app = angular.module('Contactcard', ['ngResource', 'ngRoute']);
+
+app.config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'partials/home.html'
+    });
+}]);
