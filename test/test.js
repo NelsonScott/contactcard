0f8670daf6ff891ffe55df04a21c5767b888e0e4
Scott Nelson <Scott.D.Nelson@dartmouth.edu>
2016-04-22 23:38:09 -0700

    Tear down after each test. Add tests for GET and DELETE

--- test/test.js
+++ test/test.js
@@ -5,8 +5,19 @@ var chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 var app = require('../app');
 var should = chai.should();
+var monk = require('monk');
+var db = monk('localhost:27017/test');
+var cards = db.get('cards');
 
 describe('Cards', function() {
+
+  cards.drop();
+
+  afterEach(function(done) {
+    cards.drop();
+    done();
+  });
+
   it('Should list all cards on /api/cards GET', function(done) {
     chai.request(app).
     get('/api/cards').
@@ -17,7 +28,28 @@ describe('Cards', function() {
       done();
     });
   });
-  it('Should list one cards on /api/cards/:id GET');
+
+  it('Should list one cards on /api/cards/:id GET', function(done) {
+    cards.insert({
+      name: 'Test',
+      email: 'test@example.com',
+      github: 'testaccount'
+    }, function(err, card) {
+      if (err) throw err;
+
+      chai.request(app).
+      get('/api/cards/' + card._id).
+      end(function(err, res) {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.name.should.equal('Test');
+        res.body.email.should.equal('test@example.com');
+        res.body.github.should.equal('testaccount');
+        done();
+      });
+    });
+  });
+
   it('should add one card on /api/cards POST', function(done) {
     chai.request(app).
     post('/api/cards').
@@ -27,17 +59,29 @@ describe('Cards', function() {
       'github': 'nelsonscott'
     }).
     end(function(err, res) {
-      myRes = res;
       res.should.have.status(200);
       res.should.be.json;
-      res.body.should.be({
-        'name': 'Scott',
-        'email': 'scott@scottdavidnelson.io',
-        'github': 'nelsonscott'
+      res.body.name.should.equal('Scott');
+      res.body.email.should.equal('scott@scottdavidnelson.io');
+      res.body.github.should.equal('nelsonscott');
+      done();
+    });
+  });
+
+  it('should delete a card on /api/card/:id DELETE', function(done) {
+    cards.insert({
+      name: 'Test Delete',
+      email: 'testdelete@example.com',
+      github: 'testdeleteaccount'
+    }, function(err, card) {
+      if (err) throw err;
+
+      chai.request(app).
+      delete('/api/cards/' + card._id).
+      end(function(err, res) {
+        res.should.have.status(200);
+        done();
       });
     });
-    done();
   });
-  it('should update a card on /api/card/:id PUT');
-  it('should delete a card on /api/card/:id DELETE');
 });
